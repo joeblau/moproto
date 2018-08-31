ac847485c1e33f09c853896e0c55f55c096b58fd
Joe Blau <josephblau@gmail.com>
2018-08-31 19:05:51 -0400

    Initial Commit

---
+++ Moproto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moproto
+//
+//  Created by Joe Blau on 8/31/18.
+//  Copyright © 2018 Joe Blau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
